7b4963bd64eabf69453d2a527c848d37e936732b
MajitoCan <mariajose.can23@gmail.com>
2024-02-27 13:49:12 -0600

    Se añadio el Normalize y primeros estilos con nav

--- DeveloperPage/py.py
+++ DeveloperPage/py.py
@@ -5,4 +5,5 @@ import random
 for i in range(2500):
     time.sleep(3)
     pg.write(random.choice(["Comerte una pizza", "Correr por el campo", "Hacer algo productivo.", "Intentar","hablar con alguién.", "Socializar en Discord.", "Escuchar musica. (sin saltarte.)", "Hablar con tu hermano.", "Preguntar por tu abuela", "Hablar con tus padres.", "Intentar escribir una historia.", "Mejorar tu fightrole.", "Dormir temprano.", "Ahorarrar para una pc decente."]))
-    pg.press("enter") 
+    pg.press("enter")
+    
